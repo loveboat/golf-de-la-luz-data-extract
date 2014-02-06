065227d7dfa658a1c89f4d66a1cafc83382f4f2b
David Lusby <lusbyd@gmail.com>
2014-02-06 17:08:07 +0000

    Make the script runable from the command line (and other scripts as a nice freebie)

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
 import fileinput
-from sys import argv
+import sys
 
-script, filename = argv
+def main(filename):
 
-# some vars to hold everything in place
-processing = False
-key = ''
-files = []
-data = {}
+	# some vars to hold everything in place
+	processing = False
+	key = ''
+	files = []
+	data = {}
 
+	for line in fileinput.input(filename):
 
-#for line in fileinput.input("example.txt"):
-for line in fileinput.input(filename):
+		text = line.strip(' \r\n') # strip space & new line chars
+		
+		if 'validate_name:' in text:
+			processing = True 
 
-	text = line.strip(' \r\n') # strip space & new line chars
-	
-	if 'validate_name:' in text:
-		processing = True 
+		if not processing:
+			continue
 
-	if not processing:
-		continue
+		# add break when we get to "Visitor tracking information:"
+		if "Visitor tracking information:" in text:
+			break
 
-	# add break when we get to "Visitor tracking information:"
-	if "Visitor tracking information:" in text:
-		#print "BAIL!!"
-		break
+		# pick out each key and cache data against it (until the next key)
+		if text.endswith(':'):
+			key = text
+			data[key] = ''
+		else: # key != '':
+			data[key] += text
 
-	#print text
+	files.append(data)
 
-	# pick out each key and cache data against it (until the next key)
-	if text.endswith(':'):
-		key = text
-		data[key] = ''
-	else: # key != '':
-		data[key] += text
+	for item in files:
+		print "%s,%s,%s,%s,%s" % (item['validate_name:'],
+								item['tel:'],
+								item['validate_email:'],
+								item['how_hear:'],
+								item['other_detail:'])
 
-#print data
-files.append(data)
-
-for item in files:
-	print "%s,%s,%s,%s,%s" % (item['validate_name:'],
-							item['tel:'],
-							item['validate_email:'],
-							item['how_hear:'],
-							item['other_detail:'])
+if __name__ == "__main__":
+    main(sys.argv[1])
